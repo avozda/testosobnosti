247c1407feefff0bfbea8afaa6f5282790366292
adamvozda <adam.vozda@b2a.cz>
2022-12-05 13:46:12 +0100

    api change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.response.use(
-    res => res,
-    err => {
-        return Promise.reject(err);
-    }
+  (res) => res,
+  (err) => {
+    return Promise.reject(err);
+  }
 );
 
 export default api;
